2848914d36eff85c26fb7f901763769302673914
Natalia <111825309+NaatyG@users.noreply.github.com>
2023-05-19 19:57:24 -0300

    Add files via upload

---
+++ projeto1.rb
@@ -0,0 +1,10 @@
+# Desafio: Criar um programa que dê a saída do nome completo
+# e da idade do usuário, recebendo esses dados
+
+print "Digite seu nome completo: "
+nome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "A #{nome} tem #{idade} anos."
